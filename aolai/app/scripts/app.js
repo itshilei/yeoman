14d7f70183212a32b89829679b19fc1291c76ae9
shi <itshilei@gmail.com>
2015-04-22 23:45:41 +0200

    yeoman git init

---
+++ aolai/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aolaiApp
+ * @description
+ * # aolaiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aolaiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ aolai/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aolaiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the aolaiApp
+ */
+angular.module('aolaiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
